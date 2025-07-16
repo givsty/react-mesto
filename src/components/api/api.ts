8c88e761c86829c94f3a8ab85e1e9c5a4bab2e3a
Igor <golovkovviggreserv@gmail.com>
2025-07-16 04:50:01 +0300

    fix: получение карточек вынесено в отдельную функцию

--- src/components/api/api.ts
+++ src/components/api/api.ts
@@ -1,38 +1,117 @@
-import { useEffect } from "react"
-import type { Method } from "../types/types"
+const config = {
+  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-mag-4",
+  headers: {
+    authorization: "561bff47-1094-4520-9c65-f40457c0b35c",
+    "Content-Type": "application/json",
+  },
+};
 
-interface apiProps {
-  method: Method
-}
-
-//TODO доделать
-export const api = ({method}: apiProps) => {
-  const config = {
-    baseUrl: "https://mesto.nomoreparties.co/v1/cohort-mag-4",
-    headers: {
-      authorization: "561bff47-1094-4520-9c65-f40457c0b35c",
-      "Content-Type": "application/json",
-    },
-  };
-
-  const getResponseData = (result: Response)=>{
-    result ? result.json() : Promise.reject(`Ошибка`) 
+function getResponseData(result: Response) {
+  if (result.ok) {
+    return result.json();
   }
-
-  useEffect(()=>{
-    switch (method) {
-      case 'GET':
-        fetch(`${config.baseUrl}/cards`).then((result)=>{
-          getResponseData(result)
-        }).catch((error)=>{
-          console.log(error)
-        })
-      break
-      case 'POST':
-  
-      default:
-        break;
-    }
-  }, [])
- 
+  return Promise.reject(`Ошибка ${result.status}`);
 }
+
+export function getInitialCards() {
+  return fetch(`${config.baseUrl}/cards`, {
+    headers: config.headers,
+  }).then((result) => {
+    return getResponseData(result);
+  });
+}
+
+// export function postInitialCard(cardElement) {
+//   return fetch(`${config.baseUrl}/cards`, {
+//     headers: {
+//       authorization: config.headers.authorization,
+//       "Content-Type": "application/json",
+//     },
+//     method: "POST",
+//     body: JSON.stringify({
+//       name: cardElement.name,
+//       link: cardElement.link,
+//     }),
+//     headers: config.headers,
+//   }).then((result) => {
+//     return getResponseData(result);
+//   });
+// }
+
+// export function getProfileName() {
+//   return fetch(`${config.baseUrl}/users/me`, {
+//     headers: {
+//       authorization: config.headers.authorization,
+//       "Content-Type": "application/json",
+//     },
+//     method: "GET",
+//   }).then((result) => {
+//     return getResponseData(result);
+//   });
+// }
+
+// export function patchProfileName(data) {
+//   return fetch(`${config.baseUrl}/users/me`, {
+//     method: "PATCH",
+//     headers: {
+//       authorization: config.headers.authorization,
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       name: data.name,
+//       about: data.about,
+//     }),
+//   }).then((result) => {
+//     return getResponseData(result);
+//   });
+// }
+
+// export function putLikes(_id) {
+//   return fetch(`${config.baseUrl}/cards/likes/${_id}`, {
+//     method: "PUT",
+//     headers: {
+//       authorization: config.headers.authorization,
+//     },
+//   }).then((result) => {
+//     return getResponseData(result);
+//   });
+// }
+
+// export function deletLikes(_id) {
+//   return fetch(`${config.baseUrl}/cards/likes/${_id}`, {
+//     method: "DELETE",
+//     headers: {
+//       authorization: config.headers.authorization,
+//       "Content-Type": "application/json",
+//     },
+//   }).then((result) => {
+//     return getResponseData(result);
+//   });
+// }
+
+// export function postAvatarImage(image: string) {
+//   return fetch(`${config.baseUrl}/users/me/avatar`, {
+//     headers: {
+//       authorization: config.headers.authorization,
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       avatar: image,
+//     }),
+//     method: "PATCH",
+//   }).then((result) => {
+//     return getResponseData(result);
+//   });
+// }
+
+// export function deleteInitialCard(id) {
+//   return fetch(`${config.baseUrl}/cards/${id}`, {
+//     headers: {
+//       authorization: config.headers.authorization,
+//       "Content-Type": "application/json",
+//     },
+//     method: "DELETE",
+//   }).then((result) => {
+//     return getResponseData(result);
+//   });
+// }
--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -2,30 +2,20 @@ import React, { useEffect, useState } from "react";
 import Profile from "../profile/Profile";
 import Places from "../places/Places";
 import type { Items } from "../types/types";
-import { config } from "../../utils/configApi";
+import { getInitialCards } from "../api/api";
+
 // import { config } from "../../utils/configApi";
 interface ContentProps {
   modalOpen: () => void;
-  items: Items[]
-  setItems: (items: Items[])=> void
+  items: Items[];
+  setItems: (items: Items[]) => void;
 }
 
 const Content: React.FC<ContentProps> = ({ modalOpen, setItems, items }) => {
-
   useEffect(() => {
-    fetch(`${config.baseUrl}/cards`, {
-      headers: config.headers,
-    })
-      .then((result) => {
-        return result.json();
-      })
-      .then((res) => {
-        setItems(res);
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    getInitialCards().then((res) => {
+      setItems(res as Items[]);
+    });
   }, []);
 
   return (
